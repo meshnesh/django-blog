10276323514014bc1c919bf86f04710aee390f37
CKiilu <cndeti@gmail.com>
2016-03-24 19:14:56 +0300

    Teapot demo

--- jester/urls.py
+++ jester/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import blog.views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'jester.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+    url(r'^$', blog.views.index, name = "index"),
     url(r'^admin/', include(admin.site.urls)),
 ]
